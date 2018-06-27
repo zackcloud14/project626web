998caaf3c65f6c569d9ba2eed81255ae6961041a
Akshay Anil Chandra <ac.akshay1411@live.com>
2018-06-27 19:11:52 +0800

    Initial Build

---
+++ js/firebaseinit.js
@@ -0,0 +1,11 @@
+
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyBbHt4s9n7zAX0D1nedgaHLYphHsQprGA4",
+        authDomain: "testproject-4d7b5.firebaseapp.com",
+        databaseURL: "https://testproject-4d7b5.firebaseio.com",
+        projectId: "testproject-4d7b5",
+        storageBucket: "testproject-4d7b5.appspot.com",
+        messagingSenderId: "282391652578"
+      };
+      firebase.initializeApp(config);
